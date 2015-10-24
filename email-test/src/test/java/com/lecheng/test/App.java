aa51d018f42ac83b99c468f54e18fd0babb44d7d
fengqy <pengjiajun1992@sina.com>
2015-10-24 11:07:37 +0800

    develope分支

---
+++ email-test/src/test/java/com/lecheng/test/App.java
@@ -0,0 +1,11 @@
+package com.lecheng.test;
+
+/**
+ * Created by Lecheng on 2015/10/24.
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("开发分支！！！");
+    }
+}
